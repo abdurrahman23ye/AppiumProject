814c9da7e4c3f75e19828c421a8ba711fa1a21f0
abdurrahman23ye <abdurrahman.kursicin@gmail.com>
2022-09-07 20:58:14 +0300

    olmadi

--- src/test/java/Appium/Appium06ChromeNative.java
+++ src/test/java/Appium/Appium06ChromeNative.java
@@ -11,7 +11,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Set;
 
-public class Appium06Chrome {
+public class Appium06ChromeNative {
 
     @Test
     public void test() throws InterruptedException, MalformedURLException {
@@ -22,25 +22,35 @@ public class Appium06Chrome {
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Api_29");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
-        desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"chrome");
+    //    desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"chrome"); native de kaldiriliyor
         desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, "true");
         desiredCapabilities.setCapability("chromedriverExecutable", "C:\\Users\\asus\\IdeaProjects\\AppiumTurkish\\src\\Drivers\\chromedriver.exe");
+        desiredCapabilities.setCapability("appPackage","5e36aac u0 com.android.chrome");
+        desiredCapabilities.setCapability("appActivity","com.google.android.apps.chrome.Main");
 
-        //app pakage ve app activity icin 1 adb devices
+        //app pakage ve app activity icin cmd de 1 adb devices
         //2 adb shell
         //dumpsys window | grep -E "mCurrentFocus"
+
+        // oradaki iki veriyi al
+
+        //!! bu testi çaliştirdiktan sonra aldım
+
+        //5e36aac u0 com.android.chrome/com.google.android.apps.chrome.Main}
         AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
 
         Thread.sleep(4000);
 
         Set contextName=driver.getContextHandles();
 
+        System.out.println("contextName = " + contextName);
+
         for (Object contextname:contextName
              ) {
 
             Thread.sleep(4000);
 
-            if(contextname.toString().contains("CHOROMIUM")){
+            if(contextname.toString().contains("WEBVIEW")){
 
                 driver.context((String)contextname);
             }
