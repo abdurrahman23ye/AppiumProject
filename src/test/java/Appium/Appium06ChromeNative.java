b3d6af3c7ceb51a03580f0e371059bea15b1092e
abdurrahman23ye <abdurrahman.kursicin@gmail.com>
2022-09-09 20:01:05 +0300

    bitmedi

--- src/test/java/Appium/Appium06ChromeNative.java
+++ src/test/java/Appium/Appium06ChromeNative.java
@@ -64,8 +64,50 @@ public class Appium06ChromeNative {
 
         driver.get("https://www.amazon.com");
 
-        driver.findElementByAndroidUIAutomator()
+        driver.findElementByAndroidUIAutomator("UiSelector().resourceID("buraya resourceID ya da her ne ile bulunacaksa")");
+        driver.findElementByAndroidUIAutomator("UiSelector().resourceID("buraya resourceID ya da her ne ile bulunacaksa").text("buraya text yazılabilir")");
 
+        //resourceId ile
+        // driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/addButton\")").click();
+//indexle
+//        driver.findElementByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").index(0)").click();
+//        driver.findElementByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").text(\"Test\")").click();
+        driver.findElementByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").textStartsWith(\"Add\")").click();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "/Users/ayyildiz/IdeaProjects/AppiumTechpro/src/Apps/gestureTool.apk");
+        capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
+        capabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
+        capabilities.setCapability("noReset","true");
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+//add button tikladik
+        driver.findElementByAndroidUIAutomator("UiSelector().className(\"android.widget.Button\").textStartsWith(\"Add\")").click();
+
+        Thread.sleep(5000);
+
+        String isFalse = driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/done\").enabled(false)").getAttribute("enabled");
+
+        Assert.assertEquals(isFalse,"false");
+
+        MobileElement textBox = driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/gesture_name\")");
+        textBox.sendKeys("text");
+        Thread.sleep(5000);
+        MobileElement blackScreen = driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/gestures_overlay\")");
+        blackScreen.click();
+        String isTrue = driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/done\").enabled(true)").getAttribute("enabled");
+
+        Assert.assertEquals(isTrue,"true");
+
+        System.out.println("done button enabled");
+
+        driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/done\")").click();
+
+        Thread.sleep(5000);
+        //session kapat
 
 
     }
